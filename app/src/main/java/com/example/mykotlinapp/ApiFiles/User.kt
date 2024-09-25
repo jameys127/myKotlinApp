35d9672f95c6c1c813a7d8571c1696e1e18ff1ee
Jamey <jameys127@gmail.com>
2024-09-24 22:52:15 -0700

    the api is set up in AWS and now I can add users to my database

--- app/src/main/java/com/example/mykotlinapp/ApiFiles/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mykotlinapp.ApiFiles
-
-data class User(
-    val id: Int,
-    val username: String,
-    val pass: String
-)
--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -8,8 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mykotlinapp.ApiFiles.AddUserResponse
 import com.example.mykotlinapp.ApiFiles.ApiConnect
 import com.example.mykotlinapp.ApiFiles.DatabaseApi
 import com.example.mykotlinapp.ApiFiles.User
@@ -21,9 +20,6 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
 
     lateinit var btnAdd : Button
     lateinit var btnView: Button
-//    lateinit var btnSub : Button
-//    lateinit var btnMult : Button
-//    lateinit var btnDiv : Button
     lateinit var etA : EditText
     lateinit var etB : EditText
     lateinit var resultTv : TextView
@@ -36,18 +32,12 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
 
         btnAdd = findViewById(R.id.btn_add)
         btnView = findViewById(R.id.btn_view)
-//        btnSub = findViewById(R.id.btn_sub)
-//        btnMult = findViewById(R.id.btn_mult)
-//        btnDiv = findViewById(R.id.btn_div)
         etA = findViewById(R.id.et_a)
         etB = findViewById(R.id.et_b)
         resultTv = findViewById(R.id.result_tv)
 
         btnAdd.setOnClickListener(this)
         btnView.setOnClickListener(this)
-//        btnSub.setOnClickListener(this)
-//        btnMult.setOnClickListener(this)
-//        btnDiv.setOnClickListener(this)
 
 
     }
@@ -55,39 +45,54 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     override fun onClick(v: View?) {
         var a = etA.text.toString()
         var b = etB.text.toString()
-//        lateinit var result: String
         when(v?.id){
 
             R.id.btn_view -> {
-                DatabaseApi.retrofitService.getUsers().enqueue(object : Callback<String>{
-                    override fun onResponse(call: Call<String>, response: Response<String>){
-                        if(response.isSuccessful){
-                            val rawResponse = response.body()
-                            resultTv.text = rawResponse
-                        } else{
-                            Log.e("MainActivity", "Response failed with code: ${response.code()}")
-                        }
-                    }
-
-                    override fun onFailure(call: Call<String>, t: Throwable) {
-                        Log.e("MainActivity", "Failed to fetch users", t)
-                    }
-                })
+                getUsers()
+            }
+            R.id.btn_add -> {
+                AddUser(a, b)
             }
 
-//            R.id.btn_add -> {
-//                result = a + b
-//            }
-//            R.id.btn_sub -> {
-//                result = a - b
-//            }
-//            R.id.btn_mult -> {
-//                result = a * b
-//            }
-//            R.id.btn_div -> {
-//                result = a / b
-//            }
-//
         }
     }
+    private fun AddUser(username: String, password: String){
+        val call = DatabaseApi.retrofitService.addUser(username, password)
+        call.enqueue(object : Callback<AddUserResponse>{
+            override fun onResponse(call: Call<AddUserResponse>, response: Response<AddUserResponse>) {
+                    if(response.isSuccessful){
+                        val rawResponse = response.body()
+                        if(rawResponse?.status == "success"){
+                            resultTv.text = rawResponse.message
+                        }else{
+                            resultTv.text = rawResponse?.message
+                        }
+                    }
+            }
+
+            override fun onFailure(call: Call<AddUserResponse>, t: Throwable) {
+                Log.e("MainActivity", "Failed to add users", t)
+            }
+        })
+    }
+
+    private fun getUsers(){
+        DatabaseApi.retrofitService.getUsers().enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if(response.isSuccessful){
+                    val rawResponse = response.body()
+                    resultTv.text = rawResponse?.joinToString { "\n'id': ${it.id}, 'username': ${it.username}, 'pass': ${it.pass}" }
+                }else{
+                    Log.e("MainActivity", "Response failed with code: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                Log.e("MainActivity", "Failed to fetch users", t)
+            }
+        })
+    }
+
+
+
 }
